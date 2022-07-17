940c23e395f343db8a103ce5f86eb56ba4fbfc6a
Cody Stair <ccstair27@gmail.com>
2022-07-17 17:45:35 -0400

    add ability to delete movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,10 +27,16 @@ class MoviesController < ApplicationController
     redirect_to @movie
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url
+  end
+
   private
 
   def movie_params
     params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
+      permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
