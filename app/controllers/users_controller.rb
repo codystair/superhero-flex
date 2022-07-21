1e8eaf253d5e46b5072147564a9024cca1761942
Cody Stair <ccstair27@gmail.com>
2022-07-21 14:21:49 -0400

    add authorization and admin users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -13,7 +16,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user, notice: "Thanks for signing up!"
@@ -23,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -36,16 +36,20 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to movies_url, alert: "Account successfully deleted!"
   end
 
-  private
+private
 
   def user_params
     params.require(:user).
-      permit(:name, :username, :email, :password, :password_confirmation)
+      permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true,
-                    format: { with: /\S+@\S+/ },
+
+  validates :email, format: { with: /\S+@\S+/ },
                     uniqueness: { case_sensitive: false }
-  validates :username, presence: true,
-                     format: { with: /\A[A-Z0-9]+\z/i },
-                     uniqueness: { case_sensitive: false }
 end
